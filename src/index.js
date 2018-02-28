834b60671c7cb0ef8d55ce8bb9438a7ed11fc2c4
alexsbygaga <alexsbygaga@gmail.com>
2018-03-01 00:00:59 +0300

    src/index.js add solvation (1st try)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if ((str.length)/2 != Math.floor((str.length)/2)){
+        return false;
+    }
+    for (var i = 0; i<str.length; i++){
+      for (var j = 0; j<bracketsConfig.length; j++){
+        if (bracketsConfig[j][0] == str[i]){
+         for (var h = 0; h<str.length;h++){
+            if ((bracketsConfig[j][1] == str[h]) && (i - h == -1)){
+              return true;
+           }else{
+             continue;
+           }
+         }
+        }
+      }
+    }
 }
